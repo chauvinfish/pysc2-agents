8f87e90f41f8b066182001a96e828803e86b4d65
rOMANORD sTOUKSZVINA <41475196+chauvinfish@users.noreply.github.com>
2018-07-25 23:23:30 +0800

    modified to fit pysc2 updates

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from absl import flags
 from pysc2 import maps
 from pysc2.env import available_actions_printer
 from pysc2.env import sc2_env
-from pysc2.lib import stopwatch
+from pysc2.lib import stopwatch,features
 import tensorflow as tf
 
 from run_loop import run_loop
@@ -26,27 +26,28 @@ flags.DEFINE_bool("continuation", False, "Continuously training.")
 flags.DEFINE_float("learning_rate", 5e-4, "Learning rate for training.")
 flags.DEFINE_float("discount", 0.99, "Discount rate for future rewards.")
 flags.DEFINE_integer("max_steps", int(1e5), "Total steps for training.")
-flags.DEFINE_integer("snapshot_step", int(1e3), "Step for snapshot.")
+flags.DEFINE_integer("snapshot_step", int(1e2), "Step for snapshot.")
 flags.DEFINE_string("snapshot_path", "./snapshot/", "Path for snapshot.")
 flags.DEFINE_string("log_path", "./log/", "Path for log.")
 flags.DEFINE_string("device", "0", "Device for training.")
 
-flags.DEFINE_string("map", "MoveToBeacon", "Name of a map to use.")
-flags.DEFINE_bool("render", True, "Whether to render with pygame.")
+flags.DEFINE_string("map", "DefeatRoaches", "Name of a map to use.")
+flags.DEFINE_bool("render", False, "Whether to render with pygame.")
 flags.DEFINE_integer("screen_resolution", 64, "Resolution for screen feature layers.")
 flags.DEFINE_integer("minimap_resolution", 64, "Resolution for minimap feature layers.")
 flags.DEFINE_integer("step_mul", 8, "Game steps per agent step.")
 
 flags.DEFINE_string("agent", "agents.a3c_agent.A3CAgent", "Which agent to run.")
 flags.DEFINE_string("net", "fcn", "atari or fcn.")
-flags.DEFINE_enum("agent_race", None, sc2_env.races.keys(), "Agent's race.")
-flags.DEFINE_enum("bot_race", None, sc2_env.races.keys(), "Bot's race.")
-flags.DEFINE_enum("difficulty", None, sc2_env.difficulties.keys(), "Bot's strength.")
-flags.DEFINE_integer("max_agent_steps", 60, "Total agent steps.")
+flags.DEFINE_enum("agent_race", None, ['random','protoss','terran','zerg'], "Agent's race.")
+flags.DEFINE_enum("bot_race", None, ['random','protoss','terran','zerg'], "Bot's race.")
+flags.DEFINE_enum("difficulty", None, ['very_easy','easy','medium','medium_hard','hard','very_hard','cheat_vision','cheat_money','cheat_insane']
+, "Bot's strength.")
+flags.DEFINE_integer("max_agent_steps", 240, "Total agent steps.")
 
 flags.DEFINE_bool("profile", False, "Whether to turn on code profiling.")
 flags.DEFINE_bool("trace", False, "Whether to trace the code execution.")
-flags.DEFINE_integer("parallel", 16, "How many instances to run in parallel.")
+flags.DEFINE_integer("parallel", 1, "How many instances to run in parallel.")
 flags.DEFINE_bool("save_replay", False, "Whether to save a replay at the end.")
 
 FLAGS(sys.argv)
@@ -74,9 +75,12 @@ def run_thread(agent, map_name, visualize):
     bot_race=FLAGS.bot_race,
     difficulty=FLAGS.difficulty,
     step_mul=FLAGS.step_mul,
-    screen_size_px=(FLAGS.screen_resolution, FLAGS.screen_resolution),
-    minimap_size_px=(FLAGS.minimap_resolution, FLAGS.minimap_resolution),
-    visualize=visualize) as env:
+          agent_interface_format=features.AgentInterfaceFormat(
+            feature_dimensions=features.Dimensions(screen=FLAGS.screen_resolution,
+                                                   minimap=FLAGS.minimap_resolution),
+            ),
+
+          visualize=visualize) as env:
     env = available_actions_printer.AvailableActionsPrinter(env)
 
     # Only for a single player!
